f3f8bc12c480f36a0cbddd66117ed80ea9887327
frostyserge <eyemoroz@gmail.com>
2023-07-02 22:18:09 -0400

    planets index page is initiated and working

---
+++ db/create_primary_planets.sql
@@ -0,0 +1,160 @@
+INSERT INTO main_app_primaryplanet(
+        name,
+        img,
+        description,
+        distance_from_sun,
+        year_length,
+        type,
+        moons,
+        created_at
+    )
+VALUES (
+        'Mercury',
+        'https://www.sciencefriday.com/wp-content/uploads/2020/04/mercury.jpg',
+        'The smallest planet in our solar system and nearest to the Sun, Mercury is only slightly larger than Earth’s Moon. From the surface of Mercury, the Sun would appear more than three times as large as it does when viewed from Earth, and the sunlight would be as much as seven times brighter. Mercury’s surface temperatures are both extremely hot and cold. Because the planet is so close to the Sun, day temperatures can reach highs of 800°F (430°C). Without an atmosphere to retain that heat at night, temperatures can dip as low as -290°F (-180°C).',
+        29355300,
+        '88 Earth Days',
+        'Terrestrial',
+        0,
+        CURRENT_TIMESTAMP
+    );
+INSERT INTO main_app_primaryplanet(
+        name,
+        img,
+        description,
+        distance_from_sun,
+        year_length,
+        type,
+        moons,
+        created_at
+    )
+VALUES (
+        'Venus',
+        'https://www.sciencefriday.com/wp-content/uploads/2021/06/venus.jpg',
+        'It’s a cloud-swaddled planet named for a love goddess, often called Earth’s twin. But pull up a bit closer, and Venus turns hellish. Our nearest planetary neighbor, the second planet from the Sun, has a surface hot enough to melt lead. The atmosphere is so thick that, from the surface, the Sun is just a smear of light. In some ways it is more an opposite of Earth than a twin: Venus spins backward, has a day longer than its year, and lacks any semblance of seasons. It might once have been a habitable ocean world, like Earth, but that was at least a billion years ago. A runaway greenhouse effect turned all surface water into vapor, which then leaked slowly into space. The present-day surface of volcanic rock is blasted by high temperatures and pressures. Asked if the surface of Venus is likely to be life-bearing today, we can give a quick answer: a hard NO.',
+        67483200,
+        '225 Earth Days',
+        'Terrestrial',
+        0,
+        CURRENT_TIMESTAMP
+    );
+INSERT INTO main_app_primaryplanet(
+        name,
+        img,
+        description,
+        distance_from_sun,
+        year_length,
+        type,
+        moons,
+        created_at
+    )
+VALUES (
+        'Earth',
+        'https://cdn.mos.cms.futurecdn.net/FaWKMJQnr2PFcYCmEyfiTm-1920-80.jpg.webp',
+        'Our home planet is the third planet from the Sun, and the only place we know of so far that’s inhabited by living things. While Earth is only the fifth largest planet in the solar system, it is the only world in our solar system with liquid water on the surface. Just slightly larger than nearby Venus, Earth is the biggest of the four planets closest to the Sun, all of which are made of rock and metal.',
+        94506667,
+        '365 Earth Days',
+        'Terrestrial',
+        1,
+        CURRENT_TIMESTAMP
+    );
+INSERT INTO main_app_primaryplanet(
+        name,
+        img,
+        description,
+        distance_from_sun,
+        year_length,
+        type,
+        moons,
+        created_at
+    )
+VALUES (
+        'Mars',
+        'https://solarsystem.nasa.gov/gltf_embed/2372',
+        'Mars is no place for the faint-hearted. It’s dry, rocky, and bitter cold. The fourth planet from the Sun, Mars is one of Earth’s two closest planetary neighbors (Venus is the other). Mars is one of the easiest planets to spot in the night sky – it looks like a bright red point of light. Despite being inhospitable to humans, robotic explorers – like NASA’s new Perseverance rover – are serving as pathfinders to eventually get humans to the surface of the Red Planet.',
+        154353608,
+        '687 Earth Days',
+        'Terrestrial',
+        2,
+        CURRENT_TIMESTAMP
+    );
+INSERT INTO main_app_primaryplanet(
+        name,
+        img,
+        description,
+        distance_from_sun,
+        year_length,
+        type,
+        moons,
+        created_at
+    )
+VALUES (
+        'Jupiter',
+        'https://solarsystem.nasa.gov/gltf_embed/2375',
+        'Jupiter is the fifth planet from our Sun and is, by far, the largest planet in the solar system, more than twice as massive as all the other planets combined. Jupiter’s stripes and swirls are actually cold, windy clouds of ammonia and water, floating in an atmosphere of hydrogen and helium. Jupiter’s iconic Great Red Spot is a giant storm bigger than Earth that has raged for hundreds of years. Jupiter is surrounded by dozens of moons. Jupiter also has several rings, but unlike the famous rings of Saturn, Jupiter’s rings are very faint and made of dust, not ice.',
+        460945963,
+        '4,333 Earth Days',
+        'Gas Giant',
+        80,
+        CURRENT_TIMESTAMP
+    );
+INSERT INTO main_app_primaryplanet(
+        name,
+        img,
+        description,
+        distance_from_sun,
+        year_length,
+        type,
+        moons,
+        created_at
+    )
+VALUES (
+        'Saturn',
+        'https://solarsystem.nasa.gov/gltf_embed/2355',
+        'Saturn is the sixth planet from the Sun and the second-largest planet in our solar system. Like fellow gas giant Jupiter, Saturn is a massive ball made mostly of hydrogen and helium. Saturn is not the only planet to have rings, but none are as spectacular or as complex as Saturn’s. Saturn also has dozens of moons. From the jets of water that spray from Saturn’s moon Enceladus to the methane lakes on smoggy Titan, the Saturn system is a rich source of scientific discovery and still holds many mysteries.',
+        909851040,
+        '10,759 Earth Days',
+        'Gas Giant',
+        83,
+        CURRENT_TIMESTAMP
+    );
+INSERT INTO main_app_primaryplanet(
+        name,
+        img,
+        description,
+        distance_from_sun,
+        year_length,
+        type,
+        moons,
+        created_at
+    )
+VALUES (
+        'Uranus',
+        'https://solarsystem.nasa.gov/gltf_embed/2344',
+        'The seventh planet from the Sun with the third largest diameter in our solar system, Uranus is very cold and windy. The ice giant is surrounded by 13 faint rings and 27 small moons as it rotates at a nearly 90-degree angle from the plane of its orbit. This unique tilt makes Uranus appear to spin sideways, orbiting the Sun like a rolling ball. The first planet found with the aid of a telescope, Uranus was discovered in 1781 by astronomer William Herschel, although he originally thought it was either a comet or a star. It was two years later that the object was universally accepted as a new planet, in part because of observations by astronomer Johann Elert Bode.',
+        1886395775,
+        '30,687 Earth Days',
+        'Ice Giant',
+        27,
+        CURRENT_TIMESTAMP
+    );
+INSERT INTO main_app_primaryplanet(
+        name,
+        img,
+        description,
+        distance_from_sun,
+        year_length,
+        type,
+        moons,
+        created_at
+    )
+VALUES (
+        'Neptune',
+        'https://solarsystem.nasa.gov/gltf_embed/2364',
+        'Dark, cold, and whipped by supersonic winds, ice giant Neptune is the eighth and most distant planet in our solar system. More than 30 times as far from the Sun as Earth, Neptune is the only planet in our solar system not visible to the naked eye. In 2011 Neptune completed its first 165-year orbit since its discovery in 1846. Neptune is so far from the Sun that high noon on the big blue planet would seem like dim twilight to us. The warm light we see here on our home planet is roughly 900 times as bright as sunlight on Neptune.',
+        2780144120,
+        '60,190 Earth Days',
+        'Ice Giant',
+        14,
+        CURRENT_TIMESTAMP
+    );
--- main_app/models.py
+++ main_app/models.py
@@ -3,10 +3,13 @@ from django.db import models
 # Create your models here.
 class PrimaryPlanet(models.Model):
     name = models.CharField(max_length=100)
+    img = models.CharField(max_length=250)
+    description = models.CharField(max_length=1000)
     distance_from_sun = models.IntegerField(default=0)
     year_length = models.CharField(max_length=100)
     type = models.CharField(max_length=100)
     moons = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.distance_from_sun
