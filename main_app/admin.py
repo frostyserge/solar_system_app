818c743dff768077b3cd7964c63754be8ee17b6f
frostyserge <eyemoroz@gmail.com>
2023-07-04 00:41:48 -0400

    one-to-many relationship established

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import PrimaryPlanet
+from .models import PrimaryPlanet, Moon
 
 # Register your models here.
 admin.site.register(PrimaryPlanet)
+admin.site.register(Moon)
 
